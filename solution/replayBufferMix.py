d7f62f97bf1c478710d216d30fcc9256b8e38201
JerryS-15 <JerryS15@outlook.com>
2025-09-11 14:18:14 +0200

    Create tensor from np.array.

--- solution/replayBufferMix.py
+++ solution/replayBufferMix.py
@@ -41,44 +41,23 @@ class ReplayBuffer:
                 node_order.append(pad_array(np.array(o["node_order"]), FIXED_AGENT_NUM))
                 edge_order.append(pad_array(np.array(o["edge_order"]), FIXED_AGENT_NUM))
             return (
-                torch.FloatTensor(agent_attr),
-                torch.FloatTensor(forest),
-                torch.FloatTensor(adjacency),
-                torch.FloatTensor(node_order),
-                torch.FloatTensor(edge_order),
+                torch.FloatTensor(np.array(agent_attr), dtype=torch.float32),
+                torch.FloatTensor(np.array(forest), dtype=torch.float32),
+                torch.FloatTensor(np.array(adjacency), dtype=torch.float32),
+                torch.FloatTensor(np.array(node_order), dtype=torch.float32),
+                torch.FloatTensor(np.array(edge_order), dtype=torch.float32),
             )
         
-        actions = [pad_1d(np.array(list(a.values())), FIXED_AGENT_NUM, pad_value=0) for a in action]
-        all_rewards = [pad_1d(np.array(list(r.values())), FIXED_AGENT_NUM, pad_value=0) for r in all_rewards]
-        dones = [[v for k, v in d.items() if k == "__all__"] for d in done]
+        actions = np.array([pad_1d(np.array(list(a.values())), FIXED_AGENT_NUM, pad_value=0) for a in action])
+        all_rewards = np.array([pad_1d(np.array(list(r.values())), FIXED_AGENT_NUM, pad_value=0) for r in all_rewards])
+        dones = np.array([[v for k, v in d.items() if k == "__all__"] for d in done])
 
         agent_counts = [len(o[0]["agent_attr"]) for o in obs]
-        mask = [pad_1d(np.ones(n), FIXED_AGENT_NUM, pad_value=0) for n in agent_counts]
+        mask = np.array([pad_1d(np.ones(n), FIXED_AGENT_NUM, pad_value=0) for n in agent_counts])
 
         agent_attr, forest, adjacency, node_order, edge_order = process_obs(obs)
 
         next_agent_attr, next_forest, next_adjacency, next_node_order, next_edge_order = process_obs(next_obs)
-
-        # agent_attr = np.array([o[0]["agent_attr"] for o in obs])
-        # forest = np.array([o[0]["forest"] for o in obs])
-        # adjacency = np.array([o[0]["adjacency"] for o in obs])
-        # node_order = np.array([o[0]["node_order"] for o in obs])
-        # edge_order = np.array([o[0]["edge_order"] for o in obs])
-        # actions = np.array([list(a.values()) for a in action])
-        # all_rewards = np.array([list(r.values()) for r in all_rewards])
-        # # dones = np.array([list(d.values()) for d in done])
-        # dones = np.array([
-        #     [v for k, v in d.items() if k == "__all__"]
-        #     for d in done
-        # ])
-
-        # next_agent_attr = np.array([o[0]["agent_attr"] for o in next_obs])
-        # next_forest = np.array([o[0]["forest"] for o in next_obs])
-        # next_adjacency = np.array([o[0]["adjacency"] for o in next_obs])
-        # next_node_order = np.array([o[0]["node_order"] for o in next_obs])
-        # next_edge_order = np.array([o[0]["edge_order"] for o in next_obs])
-
-        # print("DEBUG----------agent_attr.shape: ", agent_attr.shape)
 
         return {
             "agent_attr": torch.FloatTensor(agent_attr),
